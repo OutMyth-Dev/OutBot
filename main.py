0b5a468af33e98389ff17f99d0ef08189d28262f
OutDev <306765009+OutMyth-Dev@users.noreply.github.com>
2026-08-23 20:18:52 +0100

    Bug & Reformat main.py
    Patched a bug and reformatted main.py.

--- main.py
+++ main.py
@@ -20,20 +20,14 @@ if not token:
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="a")
 
 
-# |===============|
-# | Intents & cogs|
-# |===============|
+# |=========|
+# | Intents |
+# |=========|
 
 
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix=None, intents=intents)
-
-await bot.load_extension("cog.general")
-await bot.load_extension("cog.rules")
-await bot.load_extension("cog.links")
-await bot.load_extension("cog.information")
-await bot.load_extension("cog.fun")
 
 # |========|
 # | Events |
@@ -47,6 +41,18 @@ async def on_ready():
     print("OutBot is ready to be used.")
     print()
     print(f"Synced {len(synced)} slash commands.")
+
+# |======|
+# | Cogs |
+# |======|
+
+
+async def cogs():
+    await bot.load_extension("cog.general")
+    await bot.load_extension("cog.rules")
+    await bot.load_extension("cog.links")
+    await bot.load_extension("cog.information")
+    await bot.load_extension("cog.fun")
 
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
