0617b99984072ea3c023d149cf217167a7cb88bd
OutDev <306765009+OutMyth-Dev@users.noreply.github.com>
2026-08-23 20:04:30 +0100

    Blackmain.py
    Ran through black

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import os
 from discord.ext import commands
 from dotenv import load_dotenv
 
-
 # |===============|
 # | Discord cofig |
 # |===============|
@@ -16,7 +15,7 @@ load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 if not token:
     raise RuntimeError("Discord token not found.")
-    
+
 
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="a")
 
