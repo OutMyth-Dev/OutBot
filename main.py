5fa032284c97ee037b471c121df70116dfe0bfa0
OutMyth-Dev <mythordian@proton.me>
2026-08-24 19:05:45 +0100

    Fixed cogs bug and reformatted main.py to be more readable

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 import logging
 import os
 
-from cogs import cogs
+from extensions import cogs
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -11,36 +11,52 @@ from dotenv import load_dotenv
 # |===============|
 
 load_dotenv()
-token = os.getenv("DISCORD_TOKEN")
-if not token:
-    raise RuntimeError("Discord token not found.")
+discord_token = os.getenv("DISCORD_TOKEN")
+if not discord_token:
+    raise RuntimeError("Discord token not found. Please enter you discod bot's token.")
 
-handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="a")
+handler = logging.FileHandler(
+    filename="discord.log",
+    encoding="utf-8",
+    mode="a",
+)
 
 # |======|
 # | Cogs |
 # |======|
 
-for cog in cogs:
-    await bot.load_extention(cogs)
+class OutBot(commands.Bot):
 
-# |=========|
-# | Intents |
-# |=========|
+    async def setup_hook(self):
+        for cog in cogs:
+            await self.load_extension(cog)
+
+
+# |========|
+# | Intents|
+# |========|
 
 intents = discord.Intents.default()
-bot = outbot(command_prefix=None, intents=intents)
 
-# |========|
-# | Events |
-# |========|
+bot = OutBot(
+    command_prefix=None,
+    intents=intents
+)
+
+# |======|
+# |Events|
+# |======|
 
 @bot.event
 async def on_ready():
+
     synced = await bot.tree.sync()
-    print()
-    print("OutBot is ready.")
-    print()
+
+    print("\nOutBot is ready.\n")
     print(f"Synced {len(synced)} /commands.")
 
-bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+bot.run(
+    discord_token,
+    log_handler=handler,
+    log_level=logging.DEBUG,
+)
