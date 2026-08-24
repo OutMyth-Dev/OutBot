aabf806978d9b4c27c1c2eda5a5608c6e31a2fd8
OutMyth-Dev <mythordian@proton.me>
2026-08-24 15:45:44 +0100

    Spacing fix in cogs/fun

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,7 +2,6 @@ import discord
 
 from discord.ext import commands
 
-
 class FunCommands(commands.Cog):
 
     def __init__(self, bot):
@@ -18,7 +17,6 @@ class FunCommands(commands.Cog):
             "CLICK ME ---> ||<https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1>||",
             ephemeral=True,
         )
-
 
 async def setup(bot):
     await bot.add_cog(FunCommands(bot))
