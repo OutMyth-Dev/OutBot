46cb7830c74f55cad7716ae65ec9a1fa2415f776
OutMyth-Dev <mythordian@proton.me>
2026-08-25 09:35:56 +0100

    Removed an import, cleaned up main.py. REmoved useless code. Better variable names.

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import discord
 import logging
 import os
 
-from extension import extensions
-from discord.ext import commands
+from extensions import extensions
 from dotenv import load_dotenv
 
 # |===============|
@@ -11,13 +10,15 @@ from dotenv import load_dotenv
 # |===============|
 
 load_dotenv()
-discord_token = os.getenv("DISCORD_TOKEN")
-if not discord_token:
+os.getenv("DISCORD_TOKEN")
+
+if not DISCORD_TOKEN:
     logging.exception("Discord token is none. Please enter you discord bot's token.")
     raise RuntimeError("Discord token not found. Please enter you discod bot's token.")
+    print("Enter your discord bot's token.")
 
 handler = logging.FileHandler(
-    filename="discord.log",
+    filename="outbot_discord.log",
     encoding="utf-8",
     mode="a",
 )
@@ -51,13 +52,17 @@ bot = OutBot(
 @bot.event
 async def on_ready():
 
-    synced = await bot.tree.sync()
+    commands_synced = await bot.tree.sync()
 
     print("\nOutBot is ready.\n")
-    print(f"Synced {len(synced)} /commands.")
+    print(f"Synced {len(commands_synced)} /commands.")
+
+# |===========|
+# | Bot Start |
+# |===========|
 
 bot.run(
-    discord_token,
+    DISCORD_TOKEN,
     log_handler=handler,
     log_level=logging.DEBUG,
 )
