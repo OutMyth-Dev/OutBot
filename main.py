d02e02d200883204c5e7c418a99d100031bc4318
OutDev <306765009+OutMyth-Dev@users.noreply.github.com>
2026-08-26 13:38:20 +0100

    Replaced runtime error with a print statement

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 if not DISCORD_TOKEN:
     logger.error("Discord token was not found.")
-    raise RuntimeError(
+    print(
         "Discord token was not found. Please enter your Discord bot's token."
     )
 
