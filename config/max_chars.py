843866eb0b31e8342c106be1307bbf641cd12697
OutDev <306765009+OutMyth-Dev@users.noreply.github.com>
2026-08-26 11:46:47 +0100

    Fixed a grammatical error.

--- config/max_chars.py
+++ config/max_chars.py
@@ -6,4 +6,4 @@ MAX_QUESTION_LENGTH = 500
 MAX_MESSAGE_LENGTH_SENTANCE = " Please make it less than characters 1999."
 MAX_QUESTION_LENGTH_SENTANCE = " Please make it less than 500 characters."
 MAX_TITLE_LENGTH_SENTANCE = " Please make it less than 50 characters."
-MAX_TITLE_AND_QUESTION_LENGTH_SENTANCE = " Please make you title under 50 characters and your question is under 500 characters."
+MAX_TITLE_AND_QUESTION_LENGTH_SENTANCE = " Please make you title under 50 characters and your question under 500 characters."
