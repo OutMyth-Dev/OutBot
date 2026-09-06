febc83c95d0cdb60e5f297f0859e80f667aa4f2d
OutDev <306765009+OutMyth-Dev@users.noreply.github.com>
2026-09-06 17:31:22 +0100

    Created a test for the command developer which can be found in the cog cogs/developer_cog.py.

---
+++ cogs/__init__.py
@@ -0,0 +1,8 @@
+from .developer_cog import DeveloperCommands
+from .fun_cog import FreeNitroButton, FunCommands
+from .general_cog import GeneralCommands, PingUserButton
+from .information_cog import InformationCommands
+from .links_cog import LinksCommands
+from .privacy_cog import PrivacyCommands
+from .rules_cog import RulesCommands
+from .support_cog import SupportCommands
---
+++ tests/tests_developer_cog.py
@@ -0,0 +1,25 @@
+import pytest
+from unittest.mock import AsyncMock, MagicMock
+
+from cogs import DeveloperCommands
+from config import DEVELOPER, DISCORD_SERVER_INVITE_LINK
+
+@pytest.mark.asyncio
+async def test_developer():
+    developer_cog = DeveloperCommands(MagicMock())
+
+    interaction = MagicMock()
+    interaction.response.send_message = AsyncMock()
+
+    await developer_cog.developers.callback(developer_cog, interaction)
+
+    interaction.response.send_message(
+        title="OutBot's Developers",
+        description=f"{DEVELOPER} is the only developer for OutBot currently.",
+        # 0xFF0000 is Red
+        colour=0xFF0000,
+    )
+    interaction.response.send_message.set_footer(
+        text=f"You can apply here: {DISCORD_SERVER_INVITE_LINK}"
+    )
+    
