879447514d47eb821c3380eecd7270313d94e30f
OutDev <306765009+OutMyth-Dev@users.noreply.github.com>
2026-09-06 20:24:56 +0100

    Removed removed some emojis from emojis.py. Removed the comment from emojis.py.

--- config/emojis.py
+++ config/emojis.py
@@ -1,16 +1,6 @@
-# 10 reactions allow the user to pick a reaction.
-# Only 10 emojis are added so users still have the ability to add 10 of their own.
-
-
 EMOJIS: tuple(str) = (
     "👍",
     "👎",
     "✅",
     "❌",
-    "😭",
-    "💀",
-    "😂",
-    "🔥",
-    "💩",
-    "🐐",
 )
